d965098bab80ee0a1eae629363c203d56c833da2
Arvind <varvind@tamu.edu>
2020-09-24 22:18:29 -0500

    added java project

--- createTable.py
+++ createTable.py
@@ -10,27 +10,25 @@ cur = conn.cursor()
 with open ("yelp_academic_dataset_business.csv") as csvfile:
   csvreader = csv.reader(csvfile)
   lines = list(csvreader)
-  columns = "(attributes_id,business_id,"
-  for x in range(12, len(lines[0])):
-    if "-" in lines[0][x] :
-      lines[0][x] = lines[0][x].replace("-", "_")
-    if "." in lines[0][x] :
-      lines[0][x] = lines[0][x].replace(".", "_")
-
-    if lines[0][x] != "attributes" and lines[0][x] != "hours" :
-      if "attributes" in lines[0][x]:
-        lines[0][x] = lines[0][x][11:]
-      columns += lines[0][x] + ","
-  columns = columns[0:-1] + ")"
+  columns = "(business_id,"
+  for x in range(1, 12):
+    columns += lines[0][x] + ","
+  columns += "attribute_id" + ")"
+  # createTable = "CREATE TABLE business " + columns
+  # cur.execute(createTable)
   for x in range(1, len(lines)):
-    command = "INSERT INTO attr" + columns + " VALUES (\'" + str(x) + "\',\'" + lines[x][0] + "\',"
-    for y in range(12, len(lines[0])):
-      if y != 29 and y != 30:
-        if lines[x][y] == "":
-          lines[x][y] = "N/A"
-        lines[x][y] = "\'" + lines[x][y] + "\'"
-        command += lines[x][y] + ","
-    command = command[0:-1] + ")"
+    command = "INSERT INTO business" + columns + " VALUES ("
+    for y in range(0, 12):
+      if y == 11:
+        lines[x][y] = "{" + lines[x][y] + "}"
+      if lines[x][y] == "":
+        lines[x][y] = "N/A"
+      if "\'" in lines[x][y]:
+        lines[x][y] = lines[x][y].replace('\'', '\'\'')
+      lines[x][y] = "\'" + lines[x][y] + "\'"
+      command += lines[x][y] + ","
+    command +=  str(x) + ")"
+    print(x)
     cur.execute(command)
 
 
